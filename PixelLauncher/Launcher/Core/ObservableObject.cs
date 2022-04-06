88fb6f9ac2dd1c7d076d16ec95d5808edcfe7c1e
Marco Puig <dpuig2021@fau.edu>
2022-04-05 21:28:59 -0400

    Commit #1
    Base for Launcher. Features simples buttons and a basic design. Working on multiple views and more UI/UX elements.

---
+++ PixelLauncher/Launcher/Core/ObservableObject.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace PixelLauncher.Launcher.Core
+{
+    class ObservableObject : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void onPropertyChanged([CallerMemberName] string name = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+    }
+}
