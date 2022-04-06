3b5fe95c8ac13bf214fb02677b6615c1bf6548da
Marco Puig <dpuig2021@fau.edu>
2022-04-06 01:54:20 -0400

    Commit #2
    Major UI/UX improvements.
    Multiple views.
    New Settings tab (in-progress).

---
+++ PixelLauncher/Launcher/MVVN/ViewModel/MainViewModel.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using PixelLauncher.Launcher.Core;
+
+namespace PixelLauncher.Launcher.MVVN.ViewModel
+{
+    class MainViewModel : ObservableObject
+    {
+       public RelayCommand HomeViewCommand { get; set; }
+       public RelayCommand SettingsViewCommand { get; set; }
+       public HomeViewModel HomeVM { get; set; }
+       public SettingsViewModel SettingsVM { get; set; }
+
+        private object _currentView;
+
+        public object CurrentView
+        {
+            get { return _currentView; }
+            set 
+            { 
+                _currentView = value;
+                onPropertyChanged();
+            }
+        }
+
+        public MainViewModel()
+        {
+            HomeVM = new HomeViewModel();
+            SettingsVM = new SettingsViewModel();
+            CurrentView = HomeVM;
+            HomeViewCommand = new RelayCommand(o =>
+            {
+                CurrentView = HomeVM;
+            });
+            SettingsViewCommand = new RelayCommand(o =>
+            {
+                CurrentView = SettingsVM;
+            });
+        }
+    }
+}
---
+++ PixelLauncher/Launcher/MVVN/ViewModel/SettingsViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PixelLauncher.Launcher.MVVN.ViewModel
+{
+    class SettingsViewModel
+    {
+
+    }
+}
